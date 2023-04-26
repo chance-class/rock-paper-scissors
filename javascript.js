252db9a5f85eca1cbb6492002339458662253bcb
Jaime Mire <129628753+chance-class@users.noreply.github.com>
2023-04-26 16:18:36 -0500

    Update JS file with pseudocode

--- javascript.js
+++ javascript.js
@@ -1,7 +1,17 @@
+//The computer needs to randomly make a choice between Rock, Paper, or Scissors.
+
 let choices = ['Rock', 'Paper', 'Scissors'];
 
-computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  
-
+let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
 console.log(computerChoice);
+
+//Get input from the user to make a choice between Rock, Paper, or Scissors.
+
+//Compare the computer's choice and the user's choice and declare the winner.
+
+//Add 1 to the score of the winner.
+
+//Repeat 4 more times.
+
+//Alert that is the end of the game.
